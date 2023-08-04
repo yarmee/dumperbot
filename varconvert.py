f0fe9770dee5c7578e85104cbcb4fc252b121a06
yarmee <yareemaleryani200@gmail.com>
2023-08-04 23:40:39 +0300

    hell

--- varconvert.py
+++ varconvert.py
@@ -1,5 +1,12 @@
 import sys
 
 def process_data_from_javascript(data):
-    print(f"Received data from JavaScript: {data}")
+    # Access the global variable inside the function
+    global global_data
+    global_data = data
+    print(f"Received data from JavaScript: {global_data}")
+    
+   
+    
+
 
