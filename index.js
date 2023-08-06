544099cda6aef63835f54b833f3ea4946199dfbc
yarmee <yareemaleryani200@gmail.com>
2023-08-06 18:25:46 +0300

    this robot will officially not work without editing decrypt.py

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
-const { exec } = require("node:child_process");
-const fs = require("node:fs");
+const { exec } = require("child_process"); // Corrected the import for child_process
+const fs = require("fs");
+const fetch = require("node-fetch");
+const { MessageAttachment } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -22,47 +24,83 @@ client.on("message", async (message) => {
       return;
     }
 
-    // The readFile portion wasn't made in Visual Studio, rather discord. Probably works though.
-    const attachmentURL = message.attachments.first()?.url;
-
-    // Check if there are any attachments
-    if (!attachmentURL) {
+    // make sure the attachment exists
+    const attachment = message.attachments.first();
+    if (!attachment) {
       message.channel.send("No attachments found.");
       return;
     }
-    
-    // Read the file content
-    fs.readFile(attachmentURL, "utf8", (err, fileContent) => {
-      if (err) {
-        console.error("Error reading the file:", err);
-        message.channel.send("An error occurred while reading the file.");
-        return;
-      }
-    
 
-      function createTemporaryFile(fileContent, fileName) {
-        fs.writeFile(fileName, fileContent, (err) => {
+    const attachmentURL = attachment.url;
+    // Get the downloaded file name (for deletion later on)
+    const fileName = attachment.name;
+
+    try {
+      // download the file from the url
+      const response = await fetch(attachmentURL);
+
+      // create a write stream to save the file
+      const fileStream = fs.createWriteStream(fileName);
+
+      // Pipe the response body (stream) to the file stream
+      response.body.pipe(fileStream);
+
+      // make sure the file actually finished downloading fine
+      fileStream.on("finish", () => {
+        console.log("File saved successfully.");
+        fs.readFile(fileName, "utf8", (err, fileContent) => {
           if (err) {
-            console.error("Error creating temporary file:", err);
-          } else {
-            console.log("Temporary file created successfully.");
+            console.error("Error reading the file:", err);
+            message.channel.send("An error occurred while reading the file.");
+            return;
           }
+
+          // this will be used to save the file name in a temp file so the python script knows what file to delete
+          function createTemporaryFile(placeholder) {
+            fs.writeFile(fileName, fileContent, (err) => {
+              if (err) {
+                console.error("Error creating temporary file:", err);
+              } else {
+                console.log("Temporary file created successfully.");
+              }
+            });
+          }
+
+          // process the file content here (make sure it works. might be deleted later)
+          console.log("File content:", fileContent);
+
+          // execute the Python script
+          exec(`python3 decrypt.py`, (error, stdout, stderr) => {
+            if (error) {
+              console.error("Error executing the Python script:", error);
+              message.channel.send("An error occurred while extracting the webhook.");
+              return;
+            }
+            // send the succulent webhook
+            // filter it tho
+            const regexPattern = /https?:\/\/(canary|ptb|www)?\.?discord(app)?\.com\/api\/webhooks\/(\d+)\/([\w-]+)/g; // i actually want to kill myself
+            const matches = stdout.match(regexPattern);
+
+            // send the filtered message :godwhy:
+            if (matches && matches.length > 0) {
+              const resultMessage = "Here is the webhook:\n```" + matches.join("\n") + "```";
+              message.channel.send(resultMessage);
+            } else {
+              message.channel.send("No webhook found.");
+            }
+          });
         });
-      }
-
-      const fileName = "temp.exe";
-      createTemporaryFile(fileContent, fileName);
-
-      exec(`python3 decrypt.py`, (error, stdout, stderr) => {
-        if (error) {
-          console.error("Error executing the Python script:", error);
-          message.channel.send("An error occurred while decrypting the file.");
-          return;
-        }
-        // send the delicious webhook
-        message.channel.send("Here is the webhook:\n```" + stdout + "```");
       });
-    });
+
+      // something wrong on file write? it'll be here.
+      fileStream.on("error", (err) => {
+        console.error("Error saving the file:", err);
+        message.channel.send("An error occurred while saving the file.");
+      });
+    } catch (error) {
+      console.error("Error downloading the file:", error);
+      message.channel.send("An error occurred while downloading the file.");
+    }
   }
 });
 
